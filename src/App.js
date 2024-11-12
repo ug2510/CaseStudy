d463301b2bd9e1a73ca838ab97acc213cfee2c21
Urvi2510 <urvigaundalkar@gmail.com>
2024-11-12 14:43:51 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from './components/HomePage';
+import MasterView from './components/MasterView';
+import Uploader from './components/Uploader';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sec-view" element={<MasterView />} />
+          <Route path="/sec-upload" element={<Uploader />} />
+        </Routes>
       </header>
     </div>
   );
